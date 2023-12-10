5db20a4acd51abaf5b8930de8649c4f3e8fe18ee
simransin <simransingh9876rajgir@gmail.com>
2023-12-10 13:16:29 +0530

    adding new button for game

--- app.js
+++ app.js
@@ -24,5 +24,23 @@ boxes.forEach((box) => {
             turnO = true;
         }
         box.disabled = true;
+
+        checkWinner();
     });
 });
+
+const checkWinner = () => {
+    for (let pattern of winPatterns){
+        // console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
+
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("winner", pos1Val);
+            }
+        }
+    }
+}
